68b0889af1ac43763352a433086424cca2a521e9
Debarghya Bhattacharya <4084683+debarghyab@users.noreply.github.com>
2019-08-22 14:30:17 +0530

    Rate limiter incorporated to prevent DDoS attack

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var csrf = require('csurf');
 
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+// apply rate limiter to all requests
+app.use(limiter);
+
 var apiRouter = require('./routes/api');
 var usersRouter = require('./routes/users');
 
